b9f77f9685d791773508e3be49df6d090084d20c
noreyni03 <onoreyni03@gmail.com>
2025-05-05 20:33:13 +0000

    fix(ui): adding a better component structure to the landing page

---
+++ src/utils/dateUtils.ts
@@ -0,0 +1,4 @@
+export const getRandomDate = (start: Date, end: Date): string => {
+  const date = new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+  return date.toLocaleDateString('fr-FR');
+};
